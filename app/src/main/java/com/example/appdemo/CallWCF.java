a86604d5b65abf9e31a90a670f40543e6d8ea987
app_demo <a01087240961@gmail.com>
2023-11-02 17:37:16 +0900

    앱 초기 개발

--- app/src/main/java/com/example/appdemo/CallWCF.java
+++ app/src/main/java/com/example/appdemo/CallWCF.java
@@ -9,7 +9,13 @@ import  org.ksoap2.serialization.SoapPrimitive;
 public class CallWCF {
     static String ns = "http://tempuri.org/";
     static String methodname ="Reg";
+    static String methodname1 = "Login";
+    static String methodname2 = "Logout";
+    static String methodname3 = "UnReg";
+    static String soapaction1 ="http://tempuri.org/IService/Login";
     static String soapaction = "http://tempuri.org/IService/Reg";
+    static String soapaction2 = "http://tempuri.org/IService/Logout";
+    static String soapaction3 = "http://tempuri.org/IService/UnReg";
     static String url = "http://192.168.0.4/HomeworkService/Service.svc";
     public String Reg(String id,String pw,String name)
     {
@@ -36,6 +42,82 @@ public class CallWCF {
             return "예외"+e.getMessage();
         }
     }
+    public String Login(String id, String pw)
+    {
+        StrictMode.ThreadPolicy policy =
+                new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            SoapObject request1 = new SoapObject(ns, methodname1);
+            request1.addProperty("id", id);
+            request1.addProperty("pw", pw);
+
+
+            SoapSerializationEnvelope envelope1 = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope1.dotNet = true;
+            envelope1.setOutputSoapObject(request1);
+
+            HttpTransportSE hts = new HttpTransportSE(url);
+            hts.call(soapaction1, envelope1);
+
+            SoapPrimitive result1 = (SoapPrimitive) envelope1.getResponse();
+            return String.valueOf(result1);
+        }
+        catch (Exception e) {
+            return "예외"+e.getMessage();
+        }
+    }
+    public String Logout(String id, String pw)
+    {
+        StrictMode.ThreadPolicy policy =
+                new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            SoapObject request2 = new SoapObject(ns, methodname2);
+            request2.addProperty("id", id);
+            request2.addProperty("pw", pw);
+
+
+            SoapSerializationEnvelope envelope2 = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope2.dotNet = true;
+            envelope2.setOutputSoapObject(request2);
+
+            HttpTransportSE hts = new HttpTransportSE(url);
+            hts.call(soapaction2, envelope2);
+
+            SoapPrimitive result1 = (SoapPrimitive) envelope2.getResponse();
+            return String.valueOf(result1);
+        }
+        catch (Exception e) {
+            return "예외"+e.getMessage();
+        }
+    }
+    public String UnReg(String id, String pw)
+    {
+        StrictMode.ThreadPolicy policy =
+                new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            SoapObject request1 = new SoapObject(ns, methodname3);
+            request1.addProperty("id", id);
+            request1.addProperty("pw", pw);
+
+
+            SoapSerializationEnvelope envelope1 = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope1.dotNet = true;
+            envelope1.setOutputSoapObject(request1);
+
+            HttpTransportSE hts = new HttpTransportSE(url);
+            hts.call(soapaction3, envelope1);
+
+            SoapPrimitive result1 = (SoapPrimitive) envelope1.getResponse();
+            return String.valueOf(result1);
+        }
+        catch (Exception e) {
+            return "예외"+e.getMessage();
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/example/appdemo/MainActivity.java
+++ app/src/main/java/com/example/appdemo/MainActivity.java
@@ -16,6 +16,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn_reg = (Button) findViewById(R.id.btn_reg);
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+        Button btn_logout = (Button) findViewById(R.id.btn_logout);
+        Button btn_unreg = (Button) findViewById(R.id.btn_unreg);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText ed_id = (EditText) findViewById(R.id.et_id);
+                EditText ed_pw = (EditText) findViewById(R.id.et_pw);
+                String id = ed_id.getText().toString();
+                String pw = ed_pw.getText().toString();
+                CallWCF cw = new CallWCF();
+                TextView tv = (TextView)findViewById(R.id.tv_result1);
+                tv.setText(cw.Login(id,pw));
+            }
+        });
         btn_reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,5 +48,30 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText ed_id = (EditText) findViewById(R.id.et_id);
+                EditText ed_pw = (EditText) findViewById(R.id.et_pw);
+                String id = ed_id.getText().toString();
+                String pw = ed_pw.getText().toString();
+                CallWCF cw = new CallWCF();
+                TextView tv = (TextView)findViewById(R.id.tv_result2);
+                tv.setText(cw.Logout(id,pw));
+            }
+        });
+        btn_unreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText ed_id = (EditText) findViewById(R.id.et_id);
+                EditText ed_pw = (EditText) findViewById(R.id.et_pw);
+                String id = ed_id.getText().toString();
+                String pw = ed_pw.getText().toString();
+                CallWCF cw = new CallWCF();
+                TextView tv = (TextView)findViewById(R.id.tv_result3);
+                tv.setText(cw.UnReg(id,pw));
+            }
+        });
+
     }
 }
